f47f44bdfe070efaa493d5ebb102986ba0f354e3
Tmy2017 <tommykao2013@gmail.com>
2022-01-23 23:04:36 +0000

    (fix) update instruction at start page

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -3,6 +3,8 @@
     <h3>Login</h3>
     <?= $this->Form->create() ?>
     <fieldset>
+    default: hihi@hihi.com / hihi <br>
+    (on gitpod) if got strange issue like CSRF, or redirect to strange route, then try 1) delete cookies of localhost, then 2) use http://localhost:8765/users/login
         <legend><?= __('Please enter your username and password') ?></legend>
         <?= $this->Form->control('email', ['required' => true]) ?>
         <?= $this->Form->control('password', ['required' => true]) ?>
